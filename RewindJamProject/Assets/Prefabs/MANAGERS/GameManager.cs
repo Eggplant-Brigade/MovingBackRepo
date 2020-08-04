5b1f6c1308c2e0101b10c4932cb65c3c7da6599f
RobZot <robzot98@gmail.com>
2020-08-04 15:37:25 +0200

    Rewind Bar

---
+++ RewindJamProject/Assets/Prefabs/MANAGERS/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float _SecondsBeforeRewind;
+
+    public static float _RewindTime;
+    private void Start()
+    {
+        _RewindTime = _SecondsBeforeRewind;
+    }
+}
