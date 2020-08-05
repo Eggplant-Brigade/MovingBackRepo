043d23ffc75f6c9e090701ac8f7e4d1b85a7a382
RobZot <robzot98@gmail.com>
2020-08-05 19:24:14 +0200

    Fixed Tutorial invisible bridge [Alpha0.3.1]

--- RewindJamProject/Assets/Prefabs/TESTING/CELLSTESTER/CellTester.cs
+++ RewindJamProject/Assets/Prefabs/TESTING/CELLSTESTER/CellTester.cs
@@ -61,7 +61,6 @@ public class CellTester : MonoBehaviour
         #endregion
     }
 
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 8)
@@ -94,10 +93,9 @@ public class CellTester : MonoBehaviour
         RaycastHit2D hit = Physics2D.Linecast(transform.position, transform.position + new Vector3(horizontal, vertical));
         GetComponent<BoxCollider2D>().enabled = true;
 
-        if (hit.transform == null || hit.collider.gameObject.layer != 8)
-        {
-            transform.position = transform.position + new Vector3(horizontal, vertical);
-        }
+
+        transform.position = transform.position + new Vector3(horizontal, vertical);
+        
     }
 
     
