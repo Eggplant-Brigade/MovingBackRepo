518439072bd3f025817abc85f179d919f3abf82b
RobZot <robzot98@gmail.com>
2020-08-08 09:17:36 +0200

    Buildable

---
+++ RewindJamProject/Assets/Prefabs/ENVIRONMENT/BlinkingTV/BlinkingObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingObject : MonoBehaviour
+{
+
+    public Sprite SpriteA;
+    public Sprite SpriteB;
+
+    float _Timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Timer += Time.deltaTime;
+
+
+    }
+}
